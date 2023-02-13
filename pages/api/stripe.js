cedd9951481003b9c71f3168ca4d61f32fcf9eb7
ttcchub <79540594+ttcchub@users.noreply.github.com>
2023-02-13 07:21:17 +0200

    Update stripe.js

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -11,8 +11,9 @@ export default async function handler(req, res) {
       const params = {
         submit_type: 'pay', 
         mode: 'payment',
-        payment_method_types: ['card'],
-        billing_address_collection: 'required',        
+//         payment_method_types: ['card'],
+//         billing_address_collection: 'required',     
+        billing_address_collection: 'auto',
         shipping_options: [
           { shipping_rate: 'shr_1MacBSLX27mVeMm892noFsut' },
           { shipping_rate: 'shr_1MadSPLX27mVeMm82Q8KjntT' },
