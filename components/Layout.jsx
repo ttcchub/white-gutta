f0579dc8bead888a42aec2579c724e95ec26158e
ttcchub <79540594+ttcchub@users.noreply.github.com>
2023-02-18 19:10:34 +0200

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 
+import Image from 'next/image'
 
 import Navbar from './Navbar';
 import Footer from './Footer';
