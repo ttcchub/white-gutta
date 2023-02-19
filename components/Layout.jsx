9be4aa7c76f20005222e8f6e3fa9a8c11d2c8e3a
ttcchub <79540594+ttcchub@users.noreply.github.com>
2023-02-19 15:29:51 +0200

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,35 +1,45 @@
 import React from 'react';
 import Head from 'next/head';
-import Link from 'next/link';
 
 import Image from 'next/image'
 
+
 import Navbar from './Navbar';
 import Footer from './Footer';
-// import  gif from '../public/styles/giv.gif';
+import  gif from '../public/styles/giv.gif';
+
+
 
 
 const Layout = ({ children }) => {
   return (
-    <div src={gif} className="gif">
-      <Head>
+    <div className="layout container">
+
+      <section className='firstSection'>        
+        <Head>
+        <div>
+            <Image className="gif"
+              src={gif} alt="gif-g"
+            />
+          </div>
+        </Head>
         <header>
           <Navbar />
         </header>
-      </Head>
-//       <div>
-//           <Image className=""
-//             src={gif} alt="gif-g"
-//           />
-//         </div>
- 
-      <main className="main-container">
-        {children}
-      </main>
+      </section>
+
+      <section className='secondSection'>
+        <main className="main-container">
+          {children}
+        </main>
+      </section>
+      <div>
       <footer>
         <Footer />
       </footer>
+      </div>
     </div>
+
   )
 }
 
